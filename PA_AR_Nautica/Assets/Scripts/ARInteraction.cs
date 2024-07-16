02456ae90482a714eabd0a52a74d53dfa2138a0b
Niclas <147914782+GarNicWahr@users.noreply.github.com>
2024-07-16 16:48:01 +0200

    InteractionMarker mit Script versehen und eingestellt

---
+++ PA_AR_Nautica/Assets/Scripts/ARInteraction.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARInteraction : MonoBehaviour
+{
+    public GameObject uiPanel; // Das UI-Panel, das geöffnet werden soll
+
+    private Animator _animator;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        // Stelle sicher, dass das UI-Panel zu Beginn inaktiv ist
+        if (uiPanel != null)
+        {
+            uiPanel.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.transform == transform)
+                    {
+                        // Wenn das Objekt berührt wird, aktiviere das UI-Panel
+                        if (uiPanel != null)
+                        {
+                            _animator.SetTrigger("onClick");
+                            uiPanel.SetActive(true);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
