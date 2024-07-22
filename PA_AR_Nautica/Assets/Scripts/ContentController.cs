2ed3b82aaa726b02c5e40ec0d6494ec46d812489
Niclas <147914782+GarNicWahr@users.noreply.github.com>
2024-07-22 18:36:51 +0200

    Markerless AR tests

--- PA_AR_Nautica/Assets/Scripts/ContentController.cs
+++ PA_AR_Nautica/Assets/Scripts/ContentController.cs
@@ -5,9 +5,10 @@ using Vuforia;
 
 public class ContentController : MonoBehaviour
 {
+    public Transform MainCamera;
     public void AlignContentToCamera(Transform objectToAlign)
     {
-        Vector3 lookAtPosition = Camera.main.transform.position - objectToAlign.position;
+        Vector3 lookAtPosition = MainCamera.position - objectToAlign.position;
         lookAtPosition.y = 0f;
         var rotation = Quaternion.LookRotation(lookAtPosition);
         objectToAlign.rotation = rotation;
